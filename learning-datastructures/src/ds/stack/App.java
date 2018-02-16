786aa99f76b45ede5567e2efa8b8470c27944c08
Siddhant Chandiwal <chandiwal.s@husky.neu.edu>
2018-02-15 20:26:45 -0500

    Stack DataStructure

---
+++ learning-datastructures/src/ds/stack/App.java
@@ -0,0 +1,24 @@
+package ds.stack;
+
+public class App {
+
+    public static void main(String [] args){
+        Stack s = new Stack(30);
+
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        s.push(40);
+        s.push(50);
+        s.push(60);
+        s.push(70);
+        s.push(80);
+        s.push(90);
+        s.push(100);
+
+        while(!s.isEmpty()){
+            Long  value = s.pop();
+            System.out.println("Stack popping now " +value);
+        }
+    }
+}
